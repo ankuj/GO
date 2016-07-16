35cd0d27270f45fcea1f85a5d12e1b81d24244d0
Luis Custodio <luis.custodio@gmail.com>
2016-07-16 21:39:00 +0200

    Adding the map component

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
+import MapView from 'react-native-maps';
 
 export class Map extends Component {
   render() {
     return (
       <View style={ styles.container }>
-        <Text style={ styles.header } >MAP!</Text>
+        <MapView
+          style={ styles.map }
+          initialRegion={{
+          latitude: 45.188529,
+          longitude: 5.724524,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+          />
       </View>
     );
   }
--- app/components/map/styles.js
+++ app/components/map/styles.js
@@ -4,6 +4,13 @@ export default StyleSheet.create({
   container: {
     flex: 1
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
   header: {
     fontSize: 50,
     textAlign: 'center',
