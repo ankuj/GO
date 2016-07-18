df05fef24971f4c5280b8ffe682dfd23aec0b576
Luis Custodio <luis.custodio@gmail.com>
2016-07-18 20:37:43 +0200

    Adding the navigation layer and starting to work on the map markers.

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -3,8 +3,21 @@ import { View, Text } from 'react-native';
 import styles from './styles';
 import MapView from 'react-native-maps';
 
+//<script
+//        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDIjGxO_grX3PyRA8vicCxcwgfAqt2Uk4M&callback=initMap">
+//</script>
+
 export class Map extends Component {
   render() {
+
+    let myLatLng = {lat: 45.18538336, lng: 5.73860023};
+
+    //let marker = new google.maps.Marker({
+    //  position: myLatLng,
+    //  map: map,
+    //  title: 'Hello World!'
+    //});
+
     return (
       <View style={ styles.container }>
         <MapView
@@ -14,9 +27,19 @@ export class Map extends Component {
           longitude: 5.724524,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
-        }}
-          />
+        }}>
+
+        </MapView>
+
+
       </View>
     );
   }
 }
+
+/*
+ //<MapView.Marker
+ //  coordinate={myLatLng}
+ //  image={require('./images/01.png')}
+ //  />
+* */
--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+//import { NavAllDay } from './components/debug/MyScene';
 import { MainSection } from './components/mainSection';
 import React, { Component } from 'react';
 
@@ -7,7 +8,25 @@ import React, { Component } from 'react';
 export class WhereDoYouGo extends Component {
   render() {
     return (
-      <Login></Login>
+
+    <Navigator
+      style={styles.container}
+      initialRoute={{id: 'login'}}
+      renderScene={this.navigatorRenderScene}/>
+
+
     );
+  }
+
+  navigatorRenderScene(route, navigator) {
+    _navigator = navigator;
+    switch (route.id) {
+      case 'login':
+        return (<Login navigator={navigator} title='login'></Login>);
+        //return (<First navigator={navigator} title="first"/>);
+      case 'main-section':
+        return (<MainSection navigator={navigator} title="main-section"/>);
+        //return (<Second navigator={navigator} title="second"/>);
+    }
   }
 }
