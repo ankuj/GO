2317726447fe8baed7ab2adfe3db3fa0155f9f56
ankuj <ankuj_123@yahoo.co.in>
2016-07-24 11:37:50 +0200

    adIntegrationIntoLatest

--- android/app/src/main/java/com/wheredoyougo/MainApplication.java
+++ android/app/src/main/java/com/wheredoyougo/MainApplication.java
@@ -13,7 +13,9 @@ import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
-import co.apptailor.googlesignin.RNGoogleSigninPackage; 
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.sbugert.rnadmob.RNAdMobPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new MapsPackage(),
-          new RNGoogleSigninPackage()
+          new RNGoogleSigninPackage(),
+          new RNAdMobPackage()
       );
     }
   };
--- app/components/mainSection.js
+++ app/components/mainSection.js
@@ -3,9 +3,14 @@ import { View, Text } from 'react-native';
 import { NavBar } from './navbar/navbar'
 import { Map } from './map/map'
 import { MapTwo } from './map/maptwo'
+import { AdMobBanner, AdMobInterstitial } from 'react-native-admob'
+
 
 export class MainSection extends Component {
   render() {
+  	  AdMobInterstitial.setAdUnitID('ca-app-pub-4784360138786078/9907153544');
+  	  AdMobInterstitial.setTestDeviceID('4D98E460AE60002CD45B347CEDAE6AAE');
+  	  AdMobInterstitial.requestAd(AdMobInterstitial.showAd);
     return (
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimensions, so the children can't expand.
@@ -23,6 +28,11 @@ export class MainSection extends Component {
         <View style={{flex: 3, backgroundColor: 'steelblue'}} >
           <Text>Chat!Chat!Chat!</Text>
         </View>
+        <AdMobBanner
+        bannerSize="banner"
+        adUnitID="ca-app-pub-4784360138786078/9907153544"
+        testDeviceID="4D98E460AE60002CD45B347CEDAE6AAE"
+        didFailToReceiveAdWithError={this.bannerError} />
 
       </View>
     );
